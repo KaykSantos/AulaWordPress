64a1b9682c33114ebb1f1313b2d7f559d1295214
Kayk Santos <80800368+KaykSantos@users.noreply.github.com>
2023-04-11 21:18:04 -0300

    att-theme

---
+++ index.php
@@ -0,0 +1,34 @@
+    <?php get_header(); ?>
+        <header>
+            <p>Atena Idiomas</p>
+        </header>
+        <main>
+            <h3>
+                Aprenda de forma fácil e divertida! <br>
+                Domine novas línguas e conquiste o mundo com a Atena Idiomas. 
+            </h3>
+            <p>
+                Desperte o poliglota que existe em você com a Atena Idiomas! 
+                Com um método de ensino inovador e professores experientes, 
+                aprender um novo idioma nunca foi tão fácil e divertido. 
+                Seja para viagens, estudos ou crescimento profissional, a 
+                Atena Idiomas tem o curso ideal para você. <br> <br>
+                Venha se surpreender com a facilidade de se comunicar em inglês, 
+                espanhol, francês, alemão ou italiano. Aprender idiomas é transformar 
+                o mundo e você pode começar agora com a Atena Idiomas.
+            </p>
+            <a href="#cadastre-se" id="cad">Cadastre-se!</a>
+            <h3 id="cadastre-se">Cadastre-se agora mesmo!</h3>
+            <form method="post">
+                <label for="name">Nome:</label>    
+                <input type="text" name="name" id="name">
+
+                <label for="email">Email:</label>
+                <input type="email" name="email" id="email">  
+                
+                <label for="password">Senha:</label>
+                <input type="password" name="password" id="password"> 
+            </form>
+        </main>
+    <?php get_footer(); ?>
+    
